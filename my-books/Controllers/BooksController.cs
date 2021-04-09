5a59abdedd8e408019049c18f2168ce3d556aeb4
CelsonF <celson338540@gmail.com>
2021-04-09 17:26:52 -0300

    Finaly module 01, with data persistence

--- my-books/Controllers/BooksController.cs
+++ my-books/Controllers/BooksController.cs
@@ -20,12 +20,45 @@ namespace my_books.Controllers
             _booksService = booksService;
         }
 
+
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _booksService.GetAllBooks();
+            
+            return Ok(allBooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var getBook = _booksService.GetBookById(id);
+
+            return Ok(getBook);
+        }
+
+
         [HttpPost("add-book")]
         public IActionResult AddBook([FromBody] BookVM book)
         {
             _booksService.AddBook(book);
             return Ok();
         }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody] BookVM book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _booksService.DeleteBookById(id);
+            return Ok();
+        }
+
     }
 
 }
